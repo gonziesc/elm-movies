a209e71691cd00a8e3ba584e976cf5187952a748
gonza <gonziesc@gmail.com>
2018-03-31 14:19:36 -0300

    naming convention

--- src/Models.elm
+++ src/Models.elm
@@ -23,16 +23,16 @@ moviesCollection : List Movie
 moviesCollection = [avengers2, toyStory3, lionKing, it]
 
 avengers2 : Movie
-avengers2 = {id= 1, poster= createPoster "../assets/Avengers_Age_Of_Ultron.png", title= "Avengers: Age of Ultron", rating= 8.3, genre= ["Action", "Adventure", "Superhero"], link= "https://www.youtube.com/watch?v=rD8lWtcgeyg"}
+avengers2 = {id= 1, poster= createPoster "./Assets/Avengers_Age_Of_Ultron.png", title= "Avengers: Age of Ultron", rating= 8.3, genre= ["Action", "Adventure", "Superhero"], link= "https://www.youtube.com/watch?v=rD8lWtcgeyg"}
 
 toyStory3 : Movie
-toyStory3 = {id= 2, poster= createPoster "../assets/Toy_Story_3_poster.png", title= "Toy Story 3", rating= 9.0, genre= ["Family", "Animated"], link= "https://www.youtube.com/watch?v=JcpWXaA2qeg"}
+toyStory3 = {id= 2, poster= createPoster "./Assets/Toy_Story_3_poster.png", title= "Toy Story 3", rating= 9.0, genre= ["Family", "Animated"], link= "https://www.youtube.com/watch?v=JcpWXaA2qeg"}
 
 lionKing : Movie
-lionKing = {id= 3, poster= createPoster "../assets/The_Lion_King.png", title= "Lion King", rating= 9.5, genre= ["Family", "Animated"], link= "https://www.youtube.com/watch?v=_ujGv5dhGfk"}
+lionKing = {id= 3, poster= createPoster "./Assets/The_Lion_King.png", title= "Lion King", rating= 9.5, genre= ["Family", "Animated"], link= "https://www.youtube.com/watch?v=_ujGv5dhGfk"}
 
 it : Movie
-it = {id= 4, poster= createPoster "../assets/it_poster.png", title= "IT", rating= 8.0, genre= ["Horror"], link= "https://www.youtube.com/watch?v=hAUTdjf9rko"}
+it = {id= 4, poster= createPoster "./Assets/it_poster.png", title= "IT", rating= 8.0, genre= ["Horror"], link= "https://www.youtube.com/watch?v=hAUTdjf9rko"}
 
 --POSTERS
 createPoster : String -> Image
